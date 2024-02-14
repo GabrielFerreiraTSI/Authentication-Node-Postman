5a54138d5774d2bb383760e9d054123029f38166
GabrielFerreiraTSI <117236024+GabrielFerreiraTSI@users.noreply.github.com>
2024-02-13 21:54:41 -0300

    app.js and server.js have been modified

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,4 @@ const app = require("./app");
 
 const port = 3000;
 
-app.listen(port, () => console.log(`Server running on URL: http://localhost:${port}`));
+app.listen(port, () => console.log(`Server running on URL http://localhost:${port}`));
