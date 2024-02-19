ae7aac7347f8b1513ccb54e0fa5047b1ff5e7b0b
GabrielFerreiraTSI <117236024+GabrielFerreiraTSI@users.noreply.github.com>
2024-02-19 15:09:22 -0300

    Update router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const secretController = require("./controllers/secretController");
+const secretMiddleware = require("./middlewares/secretMiddleware");
 
 const router = express.Router();
 
 router.get("/", secretController.searchAll);
-router.post("/register", secretController.registUser);
-router.post("/login", secretController.verifyUser);
-router.put("/editPassword", secretController.editPassword);
+router.post(
+    "/register", 
+    secretMiddleware.validateEmail, 
+    secretMiddleware.validatePassword, 
+    secretController.registUser
+);
+router.post(
+    "/login", 
+    secretMiddleware.validateEmail, 
+    secretMiddleware.validatePassword, 
+    secretController.verifyUser
+);
+router.put("/editPassword", secretMiddleware.validatePassword, secretController.editPassword);
 router.put("/editSecret", secretController.editSecret);
 router.delete("/user", secretController.deleteUser);
 
